0313721db39299de4efec58b7f5cbfdcb742f19d
RichardLeben <shiro_tan_kuro_tan@yahoo.co.jp>
2022-05-26 03:13:57 +0900

    enable to calculate not duplicate party

--- src/utils.js
+++ src/utils.js
@@ -2,32 +2,55 @@ const concatArrays = (...arrays) => {
   return arrays.reduce((previous, current) => {
     return [...previous, ...current];
   }, []);
-}
+};
 
 const getDuplicateValues = (...valuesArray) => {
   return concatArrays(...valuesArray).filter((val, idx, _valuesArray) => {
-    return _valuesArray.indexOf(val) === idx && idx !== _valuesArray.lastIndexOf(val);
+    return (
+      _valuesArray.indexOf(val) === idx && idx !== _valuesArray.lastIndexOf(val)
+    );
   });
-}
+};
 
 const getCandidateParties = (characters, duplicateCharacters) => {
-  const supportCandidateCharacters = getDuplicateValues(characters, duplicateCharacters);
+  const supportCandidateCharacters = getDuplicateValues(
+    characters,
+    duplicateCharacters
+  );
   return supportCandidateCharacters.map((dupChara) => {
     return characters.filter((chara) => chara !== dupChara);
-  })
-}
-
-const candidatePartySets = [];
-const party1 = [1,2,3,4,5];
-const party2 = [1,3,6,7,8];
-const party3 = [2,3,9,10,11];
-const duplicateCharacters = getDuplicateValues(party1, party2, party3);
-
-getCandidateParties(party1, duplicateCharacters).forEach(candidateParty1 => {
-  getCandidateParties(party2, duplicateCharacters).forEach(candidateParty2 => {
-    getCandidateParties(party3, duplicateCharacters).forEach(candidateParty3 => {
-      candidatePartySets.push([candidateParty1, candidateParty2, candidateParty3]);
-    });
   });
-});
-const result = candidatePartySets.filter((candidatePartySet)=> getDuplicateValues(...candidatePartySet).length === 0)
+};
+
+const makeComb = function (...array) {
+  const make = (arr1, arr2) => {
+    if (arr1.length === 0) {
+      return arr2;
+    }
+    return arr1.reduce((arr, v1) => {
+      arr2.forEach((v2) => {
+        const group = [].concat(v1, v2);
+        arr.push(group);
+      });
+      return arr;
+    }, []);
+  };
+
+  return array.reduce(make, []);
+};
+
+const getAllCandidateParties = function (...array) {
+  const duplicateCharacters = getDuplicateValues(...array);
+  const _array = array.map((arr) =>
+    getCandidateParties(arr, duplicateCharacters)
+  );
+  return makeComb(..._array);
+};
+
+const party1 = [1, 2, 4, 5, 6];
+const party2 = [1, 3, 7, 8, 9];
+const party3 = [2, 3, 10, 11, 12];
+const array = [party1, party2, party3];
+const _result = getAllCandidateParties(...array).filter(
+  (candidatePartySet) => getDuplicateValues(candidatePartySet).length === 0
+);
